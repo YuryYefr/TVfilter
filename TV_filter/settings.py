58c9f518753f7bfba34c5ab3d61be83c4ade516b
Dos <yuryyefr@gmail.com>
2019-06-18 12:46:27 +0300

    'minor changes'

--- TV_filter/settings.py
+++ TV_filter/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'djangotestmailuser@gmail.com'
-EMAIL_HOST_PASSWORD = "TESTFORDJANGO"
+EMAIL_HOST_USER = os.environ.get("DB_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("DB_PASS")
